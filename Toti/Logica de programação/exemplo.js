eaad97663b338c21f26918c90ac8455ea1d9799a
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-06-21 23:58:42 -0300

    add ex da toti sobre html5

--- Toti/Logica de programação/exemplo.js
+++ Toti/Logica de programação/exemplo.js
@@ -1,11 +1,26 @@
-let j = 2;
-
-for(let contador = 0; contador < j; contador++){
-    if(j < 10){
-        for(let i = 0; i < 10; i++){
-            console.log(i);
-        }
-    }
-
+function soma (a, b) {
+  return a + b
 }
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mult(a, b) {       
+    return a * b;
+}   
+
+function div(a, b) {
+  return a / b;
+}
+
+let resultado1 = soma(6, 2);
+let resultado2 = sub(6, 2);
+let resultado3 = mult(6, 2);
+let resultado4 = div(6, 2);
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
 
--- Toti/Logica de programação/sla.c++
+++ Toti/Logica de programação/sla.c++
@@ -2,14 +2,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+float media( float n1, float n2, float n3){
+    float media =(n1 + n2 + n3) /3;
 
-int j =25;
-
-for(int contador = 0; contador < j ; contador++){
-    cout << j+1;
+    return media;
 }
 
-
-return 0;
+int main(){
+    float n1, n2, n3, resultado;
+    cin >> n1 >> n2 >> n3;
+    resultado = media(n1, n2, n3);
+    cout << fixed << setprecision(2) << resultado << endl;
+    return 0;
 }
