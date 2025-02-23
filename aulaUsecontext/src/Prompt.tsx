f3b93ea5725534af9572b390fc94c9b1431d06c2
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2025-02-23 19:57:06 -0300

    alca ultima aula

--- aulaUsecontext/src/Prompt.tsx
+++ aulaUsecontext/src/Prompt.tsx
@@ -1,6 +1,10 @@
 import React, { useRef, useState } from "react";
 
-export default function Prompt(){
+type PromptProps = {
+    onPosicaoChange: (posicao:{x:number; y:number}) => void;
+}
+
+export default function Prompt({onPosicaoChange}:PromptProps){
     const [entrada, setEntrada] = useState("");
     const [saida, setSaida] = useState("");
     const refInput = useRef<HTMLInputElement | null>(null);
@@ -11,17 +15,21 @@ export default function Prompt(){
 
     function onExecutarClique(){
         setSaida(entrada);
+        const tokens = entrada.split(" ");
+        if(tokens[0] === "mover"){
+            const x = Number(tokens[1]);
+            const y = Number(tokens[2]);
+            onPosicaoChange({x,y});
+        }
         if(!refInput.current) return;
         refInput.current.select();
     }
 
     return (
-        <>
         <div>
             <input ref={refInput} value={entrada} onChange={onEntradaChange} />
             <button onClick={onExecutarClique}>Executar</button>
             <p>{saida}</p>
         </div>
-        </>
     );
 }
--- aulaUsecontext/src/Tela.tsx
+++ aulaUsecontext/src/Tela.tsx
@@ -1,26 +1,66 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 
-export default function Tela(){
+const A = 2.0;
+const F = 1.0;
+
+type TelaProps = {
+    posicao: {
+        x: number,
+        y: number
+    }
+}
+
+export default function Tela({posicao}: TelaProps) {
     const refCanvas = useRef<HTMLCanvasElement | null>(null);
-    const [posicao, setPosicao] = useState({x:150,y:75});
-    
-    useEffect(()=>{
-            const canvas = refCanvas.current;
-            if(!canvas) return;
-            const ctx = canvas.getContext("2d");
-            if(!ctx) return;
-            ctx.fillStyle = "lightblue";
-            ctx.fillRect(0,0,canvas.width, canvas.height);
-            ctx.fillStyle = "lightgreen";
-            ctx.ellipse(posicao.x,posicao.y,5,5,0,0,2*Math.PI,false);
-            ctx.stroke();
-            ctx.fill();
+    const refAnimation = useRef<number | null>(null);
+    const refMov = useRef({
+        x: 0,
+        vx: 0,
+        ax: 0,
+        y: 0,
+        vy: 0,
+        ay: 0,
+        t0: 0,
+        dt: 0
     });
 
-    
+    useEffect(() => {
+        refAnimation.current = requestAnimationFrame(desenhaQuadro);
+        return () => {
+            if (refAnimation.current) {
+                cancelAnimationFrame(refAnimation.current);
+            }
+        }
+    }, [posicao]);
 
-    return <canvas ref={refCanvas} onClick={()=>{
-        setPosicao({...posicao, x: posicao.x + 5})
-    }}></canvas>
+    function desenhaQuadro(t: number) {
+        const m = refMov.current;
+        m.dt = Math.min(t - m.t0, 32) / 1000;
+        
+        m.ax = A * (posicao.x - m.x) - F * m.vx;
+        m.vx += m.ax * m.dt;
+        m.x += m.vx * m.dt;
+
+        m.ay = A * (posicao.y - m.y) - F * m.vy;
+        m.vy += m.ay * m.dt;
+        m.y += m.vy * m.dt;
+
+        const canvas = refCanvas.current;
+        if (!canvas) return;
+        const ctx = canvas.getContext("2d");
+        if (!ctx) return;
+        ctx.fillStyle = "lightblue";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "lightgreen";
+        ctx.beginPath();
+        ctx.ellipse(m.x,  m.y, 5, 5, 0, 0, 2 * Math.PI, false);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+        m.t0 = t;
+        refAnimation.current = requestAnimationFrame(desenhaQuadro);
+    }
+
+    return <canvas ref={refCanvas}></canvas>
 
 }
